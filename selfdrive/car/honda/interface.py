b174d1bda4ef1dcc692caefb5b60ee995abfb276
Nad-Arb <nadir.zuercher@me.com>
2017-12-26 18:58:42 +0100

    esla

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -94,6 +94,7 @@ class CarInterface(object):
     if sendcan is not None:
       self.sendcan = sendcan
       self.CC = CarController(CP.enableCamera)
+    else self.sendcan = sendcan
 
     if self.CS.accord:
       # self.accord_msg = []
